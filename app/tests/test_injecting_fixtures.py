f7b84d607015a3c48564af39253a53f1d1ae24b3
Julio Briones <Julio_Briones@epam.com>
2022-07-04 12:29:50 -0500

    Finished this section.

---
+++ app/tests/test_injecting_fixtures.py
@@ -0,0 +1,30 @@
+import pytest
+from django.contrib.auth.models import (User, Group, Permission)
+
+
+@pytest.fixture
+def app_user_group(db) -> Group:
+    group = Group.objects.create(name="app_user")
+    change_user_permissions = Permission.objects.filter(
+        codename__in=["change_user", "view_user"],
+    )
+    group.permissions.add(*change_user_permissions)
+    return group
+
+
+@pytest.fixture
+def user_a(db, app_user_group: Group) -> User:
+    user = User.objects.create_user("A")
+    user.groups.add(app_user_group)
+    return user
+
+
+@pytest.fixture
+def user_b(db, app_user_group: Group) -> User:
+    user = User.objects.create_user("B")
+    user.groups.add(app_user_group)
+    return user
+
+
+def test_should_create_two_users(user_a: User, user_b: User) -> None:
+    assert user_a.pk != user_b.pk
